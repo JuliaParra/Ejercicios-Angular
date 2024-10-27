3de2e35dcacbe4a331a7b4b09c3fb196feb017a3
Julia <musicaparatodo770@gmail.com>
2024-10-27 20:02:05 +0100

    practica componente padre/hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteInlineComponent } from './componente-inline/componente-inline.component';
 import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
